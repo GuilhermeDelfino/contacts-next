922af707ca8bde09e2864cef33319db0b12a2b63
GuilhermeDelfino <guilherme.narciso@sptech.school>
2023-02-27 01:07:00 -0300

    :lipstick: feat: add more styles for input atom

--- src/app/presentation/components/atoms/input/index.tsx
+++ src/app/presentation/components/atoms/input/index.tsx
@@ -5,13 +5,15 @@ import * as S from "./styles";
 export type InputProps = HTMLAttributes<HTMLInputElement> & {
 	ref?: Ref<HTMLInputElement>;
 	value?: string;
-	type: HTMLInputTypeAttribute;
+	type?: HTMLInputTypeAttribute;
+	width?: number | string;
 };
 export const Input: React.FC<InputProps> = ({ ...props }) => {
 	return (
 		<>
 			<S.Input
 				{...props}
+				width={props.width}
 				type={props.type || "text"}
 			/>
 		</>
--- src/app/presentation/components/atoms/input/styles.tsx
+++ src/app/presentation/components/atoms/input/styles.tsx
@@ -2,8 +2,32 @@ import styled from "styled-components";
 
 export const Input = styled.input`
 	width: ${p => p.width || "100%"};
-	height: "60px";
-	box-shadow: "0 0 20px #05050522";
-	background-color: "#f1f1f1";
-	border: "1px solid #e1e1e1";
+	box-shadow: 0 2px 0px #05050526;
+	padding: 8px 16px;
+	background-color: #f5f5f5;
+	border: 1px solid #c1c1c1;
+	border-radius: 4px;
+	font-size: 20px;
+	outline: none;
+	color: #333;
+	opacity: 0.8;
+
+	&,
+	&:hover,
+	&:focus {
+		transition: all 0.2s ease;
+	}
+
+	&:hover,
+	&:focus {
+		opacity: 1;
+	}
+
+	&:focus {
+		outline: 1.5px solid #a1a1a1;
+	}
+
+	&::placeholder {
+		color: #a1a1a1;
+	}
 `;
